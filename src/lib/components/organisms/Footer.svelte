f424851a7af8a2b6ce1cb2837fb7d5c795921c0f
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-06-19 19:43:40 +0900

    ヘッダーとフッターを作成

---
+++ src/lib/components/organisms/Footer.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+
+</script>
+
+<footer class="fixed bottom-0 bg-gray-500 text-center p-2 w-full">
+  <small class="text-white">&copy; OtaKensuke</small>
+</footer>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
   import "../app.css";
+  import Footer from "$lib/components/organisms/Footer.svelte";
+  import Header from "$lib/components/organisms/Header.svelte";
 </script>
 
-<slot />
+<Header />
+
+<slot></slot>
+
+<Footer />
