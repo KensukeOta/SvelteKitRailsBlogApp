c09ed87b98c9d724c567154649611608fff7fafb
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-06-20 11:46:49 +0900

    ログアウトボタンを作成し、ログアウト処理を実装 #3

---
+++ src/lib/components/atoms/LogoutButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { axios } from "$lib/axios";
+  import { authUser } from "$lib/stores/authUser";
+
+  async function logout() {
+    await axios.post(`${import.meta.env.VITE_API_URL}/api/users/logout`);
+    goto("/login")
+    authUser.update(n => n = "");
+  }
+</script>
+
+<button on:click={logout}>ログアウト</button>
--- src/lib/stores/authUser.ts
+++ src/lib/stores/authUser.ts
@@ -1,6 +1,5 @@
 import type { Writable } from "svelte/store";
-import type { User } from "$lib/types/User";
 import { writable } from "svelte/store";
 import { persist, localStorage } from "@macfja/svelte-persistent-store";
 
-export const authUser: Writable<User> = persist(writable(), localStorage(), "authUser");
+export const authUser: Writable<any> = persist(writable(""), localStorage(), "authUser");
