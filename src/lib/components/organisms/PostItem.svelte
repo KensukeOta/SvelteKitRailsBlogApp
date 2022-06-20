16f7638002029715537b71cdf80a77a17499a9bd
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-06-20 19:02:45 +0900

    記事更新ページを作成し、該当のページへ遷移するリンクを作成 #11

--- src/lib/components/organisms/PostItem.svelte
+++ src/lib/components/organisms/PostItem.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { Post } from "$lib/types/Post";
+import PostEditLink from "../atoms/PostEditLink.svelte";
 
   export let post: Post;
 </script>
@@ -8,5 +9,8 @@
   <a href={`/posts/${post.id}`}>
     <h1 class="font-bold">{post.title}</h1>
   </a>
-  <p>by {post.user.name}</p>
+  <nav class="flex justify-between">
+    <p>by {post.user.name}</p>
+    <PostEditLink id={post.id} />
+  </nav>
 </li>
